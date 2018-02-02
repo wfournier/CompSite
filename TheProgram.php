558f3a307d5b257bd0963e57b64d866d5a179352
SOMEONELAPTOP\luxor <luxor37@gmail.com>
2018-02-02 14:06:28 -0500

    the program content

--- TheProgram.php
+++ TheProgram.php
@@ -13,210 +13,226 @@
         }
     </style>
     <?php include "Shared/HeaderProg.php"; ?>
+    <?php include "Shared/Sidenav.html"; ?>
     <div class="row">
-        <div style="padding: 20px 50px 20px 50px">
-            <h1>About the Program</h1>
-            <div style="padding: 0 30px 0 100px">
-                <p>The Legacy to Mobile (L2M) program provides a unique experience in developing and supporting computer
-                    systems
-                    to satisfy business requirements. Graduates are well trained for a career as a programmer analyst or
-                    as
-                    technical support. Students will also benefit from excellent on-site training at companies, which
-                    may
-                    lead
-                    to full-time jobs.</p>
-                <p>
-                    Our courses are constantly evolving to meet the needs of employers. Learning takes place in both the
-                    classrooms and computer labs. Our teachers bring sound knowledge and experience to the classroom.
-                    This
-                    three-year program is well-suited for students who are interested in computer programming. Champlain
-                    has
-                    become one of the few cégeps in Quebec to offer mainframe programming and, through our partnership
-                    with
-                    the well established Fresche Legacy, students will have access to mainframe resources. In addition,
-                    students will focus on IT modernization solutions, which include programming for mobile platforms
-                    such
-                    as tablets and phones.
-                </p>
+        <div class="col-xs-2"></div>
+        <div class="col-xs-2" style=" position:absolute; height: 100%; background-color: #000f29"></div>
+        <div class="col-xs-10">
+            <div class="row">
+                <div style="padding: 20px 50px 20px 50px" id="about1">
+                    <h1>About the Program</h1>
+                    <div style="padding: 0 30px 0 100px">
+                        <p>The Legacy to Mobile (L2M) program provides a unique experience in developing and supporting
+                            computer
+                            systems
+                            to satisfy business requirements. Graduates are well trained for a career as a programmer
+                            analyst or
+                            as
+                            technical support. Students will also benefit from excellent on-site training at companies,
+                            which
+                            may
+                            lead
+                            to full-time jobs.</p>
+                        <p>
+                            Our courses are constantly evolving to meet the needs of employers. Learning takes place in
+                            both
+                            the
+                            classrooms and computer labs. Our teachers bring sound knowledge and experience to the
+                            classroom.
+                            This
+                            three-year program is well-suited for students who are interested in computer programming.
+                            Champlain
+                            has
+                            become one of the few cégeps in Quebec to offer mainframe programming and, through our
+                            partnership
+                            with
+                            the well established Fresche Legacy, students will have access to mainframe resources. In
+                            addition,
+                            students will focus on IT modernization solutions, which include programming for mobile
+                            platforms
+                            such
+                            as tablets and phones.
+                        </p>
+                    </div>
+                </div>
+
+            </div>
+            <div class="row">
+
+                <div class="row semester" id="semester1">
+                    <div class="col-xs-2">
+                    </div>
+                    <div class="col-xs-10">
+
+                        <h1>Semester 1</h1>
+                        <div class="row">
+
+                            <div class="col-xs-4">
+                                <ul>
+                                    <li>French</li>
+                                    <li>English</li>
+                                    <li>Humanities</li>
+                                    <li>Physical Education</li>
+                                </ul>
+                            </div>
+                            <div class="col-xs-5">
+                                <ul>
+                                    <li>Computing Mathematics</li>
+                                    <li>Computer Fundamentals</li>
+                                    <li>Intro to OOP</li>
+                                    <li>Web Interface Design</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row semester" id="semester2">
+                    <div class="col-xs-2"></div>
+                    <div class="col-xs-10">
+                        <h1>Semester 2</h1>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <ul>
+                                    <li>French</li>
+                                    <li>English</li>
+                                    <li>Humanities</li>
+                                    <li>Complementary</li>
+                                </ul>
+                            </div>
+                            <div class="col-xs-5">
+                                <ul>
+                                    <li>Quantitative Methods fro Computer Science</li>
+                                    <li>Programming with Java</li>
+                                    <li>Web Site Planning and Implementation</li>
+                                    <li>Operating Systems and Scripting</li>
+                                    <li>Configuring, Managing and Maintaining Computers</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row semester" id="semester3">
+                    <div class="col-xs-2"></div>
+                    <div class="col-xs-10">
+                        <h1>Semester 3</h1>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <ul>
+                                    <li>English</li>
+                                    <li>Complementary</li>
+                                    <li>Physical Education</li>
+                                </ul>
+                            </div>
+                            <div class="col-xs-5">
+                                <ul>
+                                    <li>Business Fundamentals for IT</li>
+                                    <li>COBOL Programming</li>
+                                    <li>Database Design and SQL Language</li>
+                                    <li>Networking and Internet Services</li>
+                                    <li>Game Development</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row semester" id="semester4">
+                    <div class="col-xs-2"></div>
+                    <div class="col-xs-10">
+                        <h1>Semester 4</h1>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <ul>
+                                    <li>English</li>
+                                    <li>Humanities</li>
+                                    <li>Physical Education</li>
+                                </ul>
+                            </div>
+                            <div class="col-xs-5">
+                                <ul>
+                                    <li>Social Media Business Tool</li>
+                                    <li>Business Systems Analysis</li>
+                                    <li>JCL/RPG</li>
+                                    <li>Legacy System Modernization</li>
+                                    <li>Web Programming</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row semester" id="semester5">
+                    <div class="col-xs-2">
+                    </div>
+                    <div class="col-xs-10">
+                        <h1>Semester 5</h1>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <ul>
+                                    <li>Database Systems Administration</li>
+                                    <li>Software Testing and Quality Assurance</li>
+                                    <li>Building Windows and Web Applications</li>
+                                </ul>
+                            </div>
+                            <div class="col-xs-5">
+                                <ul>
+                                    <li>Final Project 1</li>
+                                    <li>Mobile Application Development</li>
+                                    <li>Career Planning</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row semester" id="semester6">
+                    <div class="col-xs-2"></div>
+                    <div class="col-xs-10">
+                        <h1>Semester 6</h1>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <ul>
+                                    <li>Emerging Technologies</li>
+                                    <li>Final Project 2</li>
+                                    <li>Comprehensive Assessment / <br>Digital Portfolio</li>
+                                </ul>
+                            </div>
+                            <div class="col-xs-5">
+                                <h3>Externship (Stage)</h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="row semester" id="skills">
+                    <div class="col-xs-2"></div>
+                    <div class="col-xs-10">
+                        <h1>
+                            The Program Provide You
+                            with Skills in:
+                        </h1>
+                        <div class="row">
+                            <div class="col-xs-4">
+                                <ul>
+                                    <li>problem solving</li>
+                                    <li>software design</li>
+                                    <li>operating systems</li>
+                                    <li>computer programming</li>
+                                    <li>client/server - multi-tiered architecture</li>
+                                    <li>object-oriented programming</li>
+                                </ul>
+                            </div>
+                            <div class="col-xs-5">
+                                <ul>
+                                    <li>mobile device programming</li>
+                                    <li>database design and management</li>
+                                    <li>mainframe application programming</li>
+                                    <li>networking</li>
+                                    <li>web development</li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
-
     </div>
-    <div class="row">
-
-        <div class="row semester" id="semester1">
-            <div class="col-xs-2">
-            </div>
-            <div class="col-xs-10">
-
-                <h1>Semester 1</h1>
-                <div class="row">
-
-                    <div class="col-xs-4">
-                        <ul>
-                            <li>French</li>
-                            <li>English</li>
-                            <li>Humanities</li>
-                            <li>Physical Education</li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-5">
-                        <ul>
-                            <li>Computing Mathematics</li>
-                            <li>Computer Fundamentals</li>
-                            <li>Intro to OOP</li>
-                            <li>Web Interface Design</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row semester" id="semester2">
-            <div class="col-xs-2"></div>
-            <div class="col-xs-10">
-                <h1>Semester 2</h1>
-                <div class="row">
-                    <div class="col-xs-4">
-                        <ul>
-                            <li>French</li>
-                            <li>English</li>
-                            <li>Humanities</li>
-                            <li>Complementary</li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-5">
-                        <ul>
-                            <li>Quantitative Methods fro Computer Science</li>
-                            <li>Programming with Java</li>
-                            <li>Web Site Planning and Implementation</li>
-                            <li>Operating Systems and Scripting</li>
-                            <li>Configuring, Managing and Maintaining Computers</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row semester" id="semester3">
-            <div class="col-xs-2"></div>
-            <div class="col-xs-10">
-                <h1>Semester 3</h1>
-                <div class="row">
-                    <div class="col-xs-4">
-                        <ul>
-                            <li>English</li>
-                            <li>Complementary</li>
-                            <li>Physical Education</li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-5">
-                        <ul>
-                            <li>Business Fundamentals for IT</li>
-                            <li>COBOL Programming</li>
-                            <li>Database Design and SQL Language</li>
-                            <li>Networking and Internet Services</li>
-                            <li>Game Development</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row semester" id="semester4">
-            <div class="col-xs-2"></div>
-            <div class="col-xs-10">
-                <h1>Semester 4</h1>
-                <div class="row">
-                    <div class="col-xs-4">
-                        <ul>
-                            <li>English</li>
-                            <li>Humanities</li>
-                            <li>Physical Education</li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-5">
-                        <ul>
-                            <li>Social Media Business Tool</li>
-                            <li>Business Systems Analysis</li>
-                            <li>JCL/RPG</li>
-                            <li>Legacy System Modernization</li>
-                            <li>Web Programming</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row semester" id="semester5">
-            <div class="col-xs-2">
-            </div>
-            <div class="col-xs-10">
-                <h1>Semester 5</h1>
-                <div class="row">
-                    <div class="col-xs-4">
-                        <ul>
-                            <li>Database Systems Administration</li>
-                            <li>Software Testing and Quality Assurance</li>
-                            <li>Building Windows and Web Applications</li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-5">
-                        <ul>
-                            <li>Final Project 1</li>
-                            <li>Mobile Application Development</li>
-                            <li>Career Planning</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row semester" id="semester6">
-            <div class="col-xs-2"></div>
-            <div class="col-xs-10">
-                <h1>Semester 6</h1>
-                <div class="row">
-                    <div class="col-xs-4">
-                        <ul>
-                            <li>Emerging Technologies</li>
-                            <li>Final Project 2</li>
-                            <li>Comprehensive Assessment / <br>Digital Portfolio</li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-5">
-                        <h3>Externship (Stage)</h3>
-                    </div>
-                </div>
-            </div>
-        </div>
-        <div class="row semester" id="skills">
-            <div class="col-xs-2"></div>
-            <div class="col-xs-10">
-                <h1>
-                    The Program Provide You
-                    with Skills in:
-                </h1>
-                <div class="row">
-                    <div class="col-xs-4">
-                        <ul>
-                            <li>problem solving</li>
-                            <li>software design</li>
-                            <li>operating systems</li>
-                            <li>computer programming</li>
-                            <li>client/server - multi-tiered architecture</li>
-                            <li>object-oriented programming</li>
-                        </ul>
-                    </div>
-                    <div class="col-xs-5">
-                        <ul>
-                            <li>mobile device programming</li>
-                            <li>database design and management</li>
-                            <li>mainframe application programming</li>
-                            <li>networking</li>
-                            <li>web development</li>
-                        </ul>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
 </main>
 
 <?php include "Shared/Footer.html"; ?>
